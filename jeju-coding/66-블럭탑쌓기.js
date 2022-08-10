39f57e3db96af885bd88adc3f01e488a71a707be
sit-or-seona <sit_or_seona@naver.com>
2022-08-10 12:35:43 +0900

    update: jeju-coding JS100제 66

--- jeju-coding/66-블럭탑쌓기.js
+++ jeju-coding/66-블럭탑쌓기.js
@@ -3,13 +3,22 @@ const rule = "ABD";
 
 let answer = [];
 
-for(let i = 0; i < tower.length; i++){
-    let tmp = '';
-    for(let j = 0; j < tower[i].length; j++){
-        if(tower[i][j] === 'A' || tower[i][j] === 'B' || tower[i][j] === 'D'){
-            tmp += tower[i][j];
+for(let i of tower){
+    if(i.includes('A')){
+        let arrA = i.split("A");
+        if(arrA[0].includes('B'||'D')) {
+            answer.push('불가능');
+            continue;
         }
-    } tmp === rule ? answer.push("가능") : answer.push("불가능")
+    }
+    if(i.includes('B')){
+        let arrB = i.split("B");
+    if(arrB[0].includes('D')){
+        answer.push('불가능');
+        continue;
+        }
+    }
+    answer.push('가능');
 }
 
 console.log(answer);
