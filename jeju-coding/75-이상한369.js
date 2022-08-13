fa682a721627cab6706c23ee8779b096ace58561
sit-or-seona <sit_or_seona@naver.com>
2022-08-13 18:04:32 +0900

    update: jeju-coding JS100제 75

--- jeju-coding/75-이상한369.js
+++ jeju-coding/75-이상한369.js
@@ -1,18 +1,10 @@
-//split("")
-//replace(/3/gi).replace(/6/gi).replace(/9/gi)
-//!== '' count++
-//filter()
-
 const n = '93';
 
 let count = 0;
 
 for(let i = 1; i <= n*1; i++){
-    let str = i + '';
-    for(let j = 0; j < str.length; j++){
-        if(str[j] !== '3' || str[j] !== '6' || str[j] !== '9') break;
-    }
-
+    let str = (i + '').replace(/3/g, '').replace(/6/g, '').replace(/9/g, '');
+    if(str === '') count++;
 }
 
 console.log(count);
