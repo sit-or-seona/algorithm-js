7f37a4781442dd55432f80700cccdd379f373eea
sit-or-seona <sit_or_seona@naver.com>
2022-08-15 23:07:27 +0900

    fix: jeju-coding JS100제 55

--- jeju-coding/55-하노이의 탑.js
+++ jeju-coding/55-하노이의 탑.js
@@ -8,11 +8,11 @@ function hanoi(num, start, end, temp){
   }
 
   //원반이 n-1개를 경유기둥으로 옮기고
-  hanoi(/*내용을 채워주세요.*/);
+  hanoi(num-1, start, temp, end);
   //가장 큰 원반은 목표기둥으로
-  route.push(/*내용을 채워주세요.*/);
+  route.push([start, end]);
   //경유기둥과 시작기둥을 바꿉니다.
-  hanoi(/*내용을 채워주세요.*/);
+  hanoi(num-1, temp, end, start);
 }
 
 hanoi(3, 'A', 'B', 'C');
