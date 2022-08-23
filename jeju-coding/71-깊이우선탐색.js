8929f849b06706b4dec8bebb42f9268d14189436
sit-or-seona <sit_or_seona@naver.com>
2022-08-23 23:15:33 +0900

    studying: jeju-coding JS100제 71깊이우선탐색

---
+++ jeju-coding/71-깊이우선탐색.js
@@ -0,0 +1,26 @@
+const graph = {'E': ['D', 'A'],
+         'F': ['D'],
+         'A': ['E', 'C', 'B'],
+         'B': ['A'],
+         'C': ['A'],
+         'D': ['E','F']};
+
+         function dfs(graph, start){
+            let visited = [];
+            let stack = [start];
+          
+            while (stack.length !== 0){
+              let n = stack.pop();
+              if (!visited.includes(n)){
+                visited.push(n);
+                let sub = graph[n].filter(x => !visited.includes(x));
+                for(let i of sub) {
+                  stack.push(i);
+                }
+              }
+            }
+            return visited;
+          }
+          
+          
+          console.log(dfs(graph, 'E'));
