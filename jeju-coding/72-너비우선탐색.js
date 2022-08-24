d928d51e312397aa7cd56b93701ff5cf39b5c44f
sit-or-seona <sit_or_seona@naver.com>
2022-08-24 18:47:01 +0900

    solve: jeju-coding JS100제 72

---
+++ jeju-coding/72-너비우선탐색.js
@@ -0,0 +1,23 @@
+const graph = {'E': ['D', 'A'],
+'F': ['D'],
+'A': ['E', 'C', 'B'],
+'B': ['A'],
+'C': ['A'],
+'D': ['E','F']};
+
+function bfs (graph, start) {
+    let answer = [];
+    let queue = [start];
+
+    while(queue.length !== 0){
+        let shift = queue.shift();
+        if(!answer.includes(shift)) answer.push(shift);
+        let sub = graph[shift].filter(i => !answer.includes(i));
+        for(let i of sub){
+            queue.push(i);
+        }
+    }
+    return answer;
+}
+
+console.log(bfs(graph, 'E'));
