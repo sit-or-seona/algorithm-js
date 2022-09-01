31048848ca3f3c3ee67514e6398e8aaa2d30874c
seona.bang <sit.or.seona@gmail.com>
2022-09-01 23:15:50 +0900

    solve: programmers lv.1 kakao2021 신규아이디추천

---
+++ programmers/신규아이디추천.js
@@ -0,0 +1,14 @@
+function solution(new_id) {
+    var answer = '';
+    
+    answer = new_id.toLowerCase()
+    .replace(/[^a-z0-9-_.]/g, '')
+    .replace(/[.]{2,}/g, '.')
+    .replace(/^[.]+/, '')
+    .replace(/[.]+$/, '')
+    .replace(/^$/,'a')
+    .slice(0,15)
+    .replace(/[.]+$/, '');
+    
+    return answer.padEnd(3, answer[answer.length-1]);
+}
