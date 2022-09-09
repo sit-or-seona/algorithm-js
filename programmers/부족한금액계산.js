c7295b3066146ce6d751cedc9ae947f5398ed204
seona.bang <sit.or.seona@gmail.com>
2022-09-09 17:56:27 +0900

    solve: programmers lv.1 부족한금액계산

---
+++ programmers/부족한금액계산.js
@@ -0,0 +1,12 @@
+function solution (price,money,count) {
+    
+    let answer = 0;
+
+    for(let i = 1; i <= count; i++){
+        answer += price * i;
+    }
+
+    return answer > money ? answer - money : 0;
+}
+
+console.log(solution(3,20,4));
