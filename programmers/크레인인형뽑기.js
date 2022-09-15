bc141f470c82d11c7259e7402548f3e9601fc43e
seona.bang <sit.or.seona@gmail.com>
2022-09-15 12:46:20 +0900

    solve: programmers lv.1 kakao2019 크레인인형뽑기

---
+++ programmers/크레인인형뽑기.js
@@ -0,0 +1,22 @@
+function solution (board, moves) {
+
+    let basket = [];
+    let result = 0;
+
+    for(let i of moves) {
+        for(let j = 0; j < board.length; j++){
+            if(board[j][i-1] !== 0) {
+                if(basket[basket.length-1] === board[j][i-1]) {
+                    basket.pop();
+                    result += 2;
+                }
+                else basket.push(board[j][i-1]);
+                board[j][i-1] = 0;
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+console.log(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4]));
