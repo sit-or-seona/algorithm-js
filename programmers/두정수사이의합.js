c18a1f65231562b53a893d4e7e4d5d0c4a28930c
seona.bang <sit.or.seona@gmail.com>
2022-10-21 23:24:55 +0900

    solve: programmers lv.1 두정수사이의합

---
+++ programmers/두정수사이의합.js
@@ -0,0 +1,14 @@
+function solution (a, b) {
+    let result = 0;
+
+    if(a > b) {
+        [a, b] = [b, a]
+    }
+
+    for(let i = a; i <= b; i++){
+        result += i;
+    }
+    return result;
+}
+
+console.log(solution(3, 5));
