91069d48dc6d3bb398f0a6c83a39922a21ad8adb
seona.bang <sit.or.seona@gmail.com>
2022-10-27 22:27:37 +0900

    fix: programmers lv.1 숫자짝꿍

--- programmers/숫자짝꿍.js
+++ programmers/숫자짝꿍.js
@@ -1,19 +1,16 @@
 function solution(X, Y) {
     let answer = '';
+    for(let i = 9; i >= 0; i--) {
+        let regexp = new RegExp(i, 'g');
+        let arrX = X.match(regexp);
+        let arrY = Y.match(regexp);
 
-    if(X.length > Y.length) {
-        [X, Y] = [Y, X];
-    }
-
-    Y = Y.split('').sort((a,b) => b-a);
-    X.split('').sort((a,b) => b-a).forEach(i => {
-        if(Y.includes(i)){
-            answer += i;
-            Y = Y.slice(Y.indexOf(i) + 1);
+        if(arrX && arrY) {
+            arrX.length <= arrY.length ? answer += arrX.join('') : answer += arrY.join('');
         }
-    })
+        
+    }
     if(answer.length < 1) return "-1"
-    
     return !!answer.replace(/0/g, '') ? answer : answer = "0";
 }
 
