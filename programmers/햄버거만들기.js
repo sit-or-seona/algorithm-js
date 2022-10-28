e59875674e984d4991fd8c4ea4304f5ddc951ffe
seona.bang <sit.or.seona@gmail.com>
2022-10-28 18:17:18 +0900

    update: programmers lv.1 햄버거만들기

--- programmers/햄버거만들기.js
+++ programmers/햄버거만들기.js
@@ -3,11 +3,15 @@ function solution(ingredient) {
     let arr = [];
     for (let i of ingredient) {
         arr.push(i);
-
         if(arr.length >= 4) {
-            if(arr[arr.length-4] === 1 && arr[arr.length-3] === 2 && arr[arr.length-2] === 3 && arr[arr.length-1] === 1) {
+            if(
+                arr[arr.length-4] === 1 && 
+                arr[arr.length-3] === 2 && 
+                arr[arr.length-2] === 3 && 
+                arr[arr.length-1] === 1
+                ) {
                 result++;
-                arr.splice(-4, 4);
+                arr.splice(-4);
             }
         }
     }
