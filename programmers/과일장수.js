4ae08936454976025b13373668674b90326b3e89
seona.bang <sit.or.seona@gmail.com>
2022-11-12 22:24:45 +0900

    solve: programmers lv.1 과일장수

---
+++ programmers/과일장수.js
@@ -0,0 +1,12 @@
+function solution(k, m, score) {
+    let answer = 0;
+    score.sort((a,b) => b-a);
+    
+    for(let i = m-1; i < score.length; i += m) {
+        answer += score[i] > k ? k * m : score[i] * m;
+    }
+    
+    return answer;
+}
+
+console.log(solution(3, 4, [1, 2, 3, 1, 2, 3, 1]));
