01a5ebf1329b3bd2d7830801ddd54359569b14d4
seona.bang <sit.or.seona@gmail.com>
2023-01-20 15:31:52 +0900

    solve: programmers lv.2 점프와순간이동

---
+++ programmers/점프와순간이동.js
@@ -0,0 +1,15 @@
+function solution(n) {
+  let answer = 1;
+
+  while (n > 1) {
+    if (n % 2 === 0) {
+      n /= 2;
+    } else {
+      n = (n - 1) / 2;
+      answer++;
+    }
+  }
+  return answer;
+}
+
+console.log(solution(6));
