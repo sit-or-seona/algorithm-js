681fb93a7d05e980aa678018fb3cd1d1e9b5ab0c
seona.bang <sit.or.seona@gmail.com>
2023-02-22 20:15:33 +0900

    solve: programmers lv.1 카드뭉치

---
+++ programmers/카드뭉치.js
@@ -0,0 +1,8 @@
+function solution(cards1, cards2, goal) {
+  for (let i of goal) {
+    if (i === cards1[0]) cards1.shift();
+    else if (i === cards2[0]) cards2.shift();
+    else return "No";
+  }
+  return "Yes";
+}
